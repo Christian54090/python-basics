d6df4b82d94ad2dd4decb8e77f37d844fbb7321b
Christian Sanders <christian.sanders@live.com>
2019-06-13 21:36:03 -0700

    Arithmetic and Strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+course = "Python's course for \"beginners\""
+print(course)
+print(course[0:-4])
+
+big_str = """
+Hi I am a string
+I can exist on multiple lines
+tadah~~
+"""
+print(big_str)
+
+name = "Jennifer"
+print(name[1:-1])
---
+++ type-conversion.py
@@ -0,0 +1,3 @@
+lbs = input("What is your weight? ")
+kg = lbs / 2.2
+print(kg)
